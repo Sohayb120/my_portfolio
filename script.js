f96e6e5c4f5a48e1cf3efdcd422dd9ab194ae41a
Souhayb Hadi <144113890+Sohayb120@users.noreply.github.com>
2024-12-31 15:28:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ document.addEventListener("DOMContentLoaded", () => {
         observer.observe(section);
     });
 });
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
